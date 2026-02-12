ba6e6d4312ab2e7f54dcb0cb7379f14c2f556e46
Josh <jjw.watson2710@gmail.com>
2026-02-12 16:06:11 +0000

    Addded responsive image on homepage

--- GroupProject/src/Pages/HomePage/Homepage.jsx
+++ GroupProject/src/Pages/HomePage/Homepage.jsx
@@ -1,16 +1,38 @@
 import styles from './Homepage.module.css'
 import WorkPlaceWellbeing from '../../assets/images/WorkPlaceWellbeing.jpg';
+import mobileWorkPlaceWellbeing from '../../assets/images/mobileWorkPlaceWellbeing.jpg';
+
+function heroBannerImage(){
+    let heroBanner;
+    if (window.innerWidth <= 750){
+        heroBanner = (
+            <>
+                <div className={styles['banner']}>
+                    <div>
+                        <img src={mobileWorkPlaceWellbeing} alt="Wellbeing" />
+                    </div>
+                </div>
+            </>
+        )
+    }
+    else{
+        heroBanner = (
+            <>
+                <div className={styles['banner']}>
+                    <div>
+                        <img src={WorkPlaceWellbeing} alt="Wellbeing" />
+                    </div>
+                </div>
+            </>
+
+        )
+
+    }
+    return heroBanner;
+}
 
 function HomePage() {
-    return (
-        <>
-            <div className={styles['banner']}>
-                <div>
-                    <img src={WorkPlaceWellbeing} alt="Wellbeing" />
-                </div>
-            </div>
-        </>
-    )
+    return heroBannerImage()
 }
 
 export default HomePage
