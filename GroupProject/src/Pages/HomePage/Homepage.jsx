7b43d2f78a8bcdb6d7470dc41f4a94d3777b9c97
Josh <jjw.watson2710@gmail.com>
2026-03-05 10:31:25 +0000

    Adding Dynamic Image slideshow

--- GroupProject/src/Pages/HomePage/Homepage.jsx
+++ GroupProject/src/Pages/HomePage/Homepage.jsx
@@ -3,34 +3,7 @@ import WorkPlaceWellbeing from '../../assets/images/WorkPlaceWellbeing.jpg';
 import mobileWorkPlaceWellbeing from '../../assets/images/mobileWorkPlaceWellbeing.jpg';
 import SlideShow from "../../Components/ImageSlideShow/ImageSlideShow.jsx";
 import ImageComponent from "../../Components/Image/ImageComponent.jsx";
-function heroBannerImage(){
-    let heroBanner;
-    if (window.innerWidth <= 750){
-        heroBanner = (
-            <>
-                <div className={styles['banner']}>
-                    <div>
-                        <img src={mobileWorkPlaceWellbeing} alt="Wellbeing" />
-                    </div>
-                </div>
-            </>
-        )
-    }
-    else{
-        heroBanner = (
-            <>
-                <div className={styles['banner']}>
-                    <div>
-                        <img src={WorkPlaceWellbeing} alt="Wellbeing" />
-                    </div>
-                </div>
-            </>
 
-        )
-
-    }
-    return heroBanner;
-}
 function SlideShowCreation(){
     const imageList = [
         <ImageComponent width={'100px'} height={'100px'} imagePath={WorkPlaceWellbeing} type={'banner'} key={'image1'}></ImageComponent>,
