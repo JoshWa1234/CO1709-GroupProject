d4837a8d97a41e544753e8e0048a046a40eb2814
Josh <jjw.watson2710@gmail.com>
2026-04-29 23:00:55 +0100

    Removed Footer

--- GroupProject/src/ui Components/Footer/Footer.jsx
+++ GroupProject/src/ui Components/Footer/Footer.jsx
@@ -1,23 +1,24 @@
 import styles from "./Footer.module.css";
 import FooterItem from "../FooterItem/FooterItem.jsx";
+import {useAuth} from "@/context/AuthContext.jsx";
 
 function Footer() {
+
+    const {user} = useAuth()
     return (
         <>
             <div className={styles['footer-container']}>
                 <div className={styles['footer-container-left']}>
-                    <div className={styles['footer-item-container']}>
-                        <FooterItem message={'Test-left'}></FooterItem>
-                    </div>
                 </div>
                 <div className={styles['footer-container-right']}>
-                    <div className={styles['footer-item-container']}>
-                        <FooterItem message={'Test-right'}></FooterItem>
-                    </div>
+                    {user === null ? (
+                        <div className={styles['footer-item-container']}>
+                            <FooterItem path={"/login"} message={'Login'}></FooterItem>
+                        </div>
+                    ):
+                    <div className={styles['footer-item-container']}></div>}
 
-                    <div className={styles['footer-item-container']}>
-                        <FooterItem path={"/login"} message={'Login'}></FooterItem>
-                    </div>
+
                 </div>
             </div>
         </>
